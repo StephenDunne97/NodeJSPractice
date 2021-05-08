ad9d6114b8d16063b7805ea29c9fcf89a9544109
StephenDunne97 <dunne134@gmail.com>
2021-05-08 17:40:32 +0100

    Update timers.js
    Logging the time as a percentage.

--- timers.js
+++ timers.js
@@ -1,17 +1,23 @@
-const waitTime = 3000; // Milliseconds
+const waitTime = 5000; // Milliseconds
 const waitInterval = 500; // Half a second
 let currentTime = 0; // Not a const as it will change
 
 const incTime = () => {
     currentTime += waitInterval;
-    console.log(`Waiting ${currentTime/1000} seconds`);
+    const p = Math.floor((currentTime/waitTime) * 100);
+    // console.log(`Waiting ${currentTime/1000} seconds`);
+    process.stdout.clearLine(); // Clears std out
+    process.stdout.cursorTo(0); // Moves cursor back to 0
+    process.stdout.write(`Waiting... ${p}%`); // Logs the time as a percentage
 };
 
 console.log(`Delay set for: ${waitTime/1000} seconds`);
 const timerFinished = () => {
     clearInterval(interval);
-    console.log("Time up!");
+    process.stdout.clearLine(); // Clears std out
+    process.stdout.cursorTo(0); // Moves cursor back to 0
+    process.stdout.write(`Time up!`); // Logs the time as a percentage
 };
 
 const interval = setInterval(incTime, waitInterval); // Calls the incTime function every .5 seconds.
-setTimeout(timerFinished, waitTime);
+setTimeout(timerFinished, waitTime); // Calls timerFinished when the waitTime is hit.
